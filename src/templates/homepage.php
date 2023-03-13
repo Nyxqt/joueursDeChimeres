c9ab586b9c74b27768e3560a7e9679dc1f56b621
Nyxqt <geiss.gamer@gmail.com>
2023-03-13 08:38:53 +0100

    homepage

--- src/templates/homepage.php
+++ src/templates/homepage.php
@@ -3,7 +3,89 @@
 
 <!-- MAIN CONTENT -->
 
-test
-
+<div class="container-fluid mb-5 p-0">
+    <div id="carousel-homepage" class="carousel slide p-0 m-0 carousel-homepage h-60" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/images/tavern.png" class="d-block w-100" alt="Taverne">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/medieval-bgt.png" class="d-block w-100" alt="Ville">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/outdoor-street.png" class="d-block w-100" alt="Rue médiévale">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-homepage" data-bs-slide="prev">
+            <span aria-hidden="true"><i class="fa-solid fa-arrow-left fa-xl"></i></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel-homepage" data-bs-slide="next">
+            <span aria-hidden="true"><i class="fa-solid fa-arrow-right fa-xl"></i></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<div class="container mt-5">
+    <div class="row text-center align-items-center p-3 mt-3 mb-3">
+        <div class="col">
+            <i class="fa-solid fa-dragon fa-2xl">
+                <span class="important-f fs-2 blur">JDR</span>
+            </i>
+        </div>
+        <div class="col">
+            <p class="text-f fs-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum hic deleniti commodi cupiditate quisquam dolorem atque vero. Error ullam odit, placeat a repellendus consequatur quo eius laborum at nulla? Aspernatur.
+            </p>
+        </div>
+        <div class="col">
+            <a class="btn btn-outline-info" href="#" role="button">Plus d'information</a>
+        </div>
+    </div>
+    <hr />
+    <div class="row text-center align-items-center p-3 mt-3 mb-3">
+        <div class="col">
+            <i class="fa-solid fa-landmark fa-2xl">
+                <span class="important-f fs-2">Association</span>
+            </i>
+        </div>
+        <div class="col">
+            <p class="text-f fs-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum hic deleniti commodi cupiditate quisquam dolorem atque vero. Error ullam odit, placeat a repellendus consequatur quo eius laborum at nulla? Aspernatur.
+            </p>
+        </div>
+        <div class="col">
+            <a class="btn btn-outline-info" href="#" role="button">Plus d'information</a>
+        </div>
+    </div>
+    <hr />
+    <div class="row text-center align-items-center p-3 mt-3 mb-3">
+        <div class="col">
+            <i class="fa-solid fa-book fa-2xl">
+                <span class="important-f fs-2">Evenement</span>
+            </i>
+        </div>
+        <div class="col">
+            <p class="text-f fs-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum hic deleniti commodi cupiditate quisquam dolorem atque vero. Error ullam odit, placeat a repellendus consequatur quo eius laborum at nulla? Aspernatur.
+            </p>
+        </div>
+        <div class="col">
+            <a class="btn btn-outline-info" href="#" role="button">Plus d'information</a>
+        </div>
+    </div>
+    <hr />
+    <div class="row text-center align-items-center p-3 mt-3 mb-3">
+        <div class="col">
+            <i class="fa-solid fa-leaf fa-2xl">
+                <span class="important-f fs-2">Actualité</span>
+            </i>
+        </div>
+        <div class="col">
+            <p class="text-f fs-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum hic deleniti commodi cupiditate quisquam dolorem atque vero. Error ullam odit, placeat a repellendus consequatur quo eius laborum at nulla? Aspernatur.
+            </p>
+        </div>
+        <div class="col">
+            <a class="btn btn-outline-info" href="#" role="button">Plus d'information</a>
+        </div>
+    </div>
+</div>
 <?php $content = ob_get_clean(); ?>
 <?php require('layout.php') ?>
--- src/templates/layout.php
+++ src/templates/layout.php
@@ -7,10 +7,25 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0 fs-5">
                 <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link title-f active background-nav" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link title-f background-nav" href="#">JDR</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link title-f background-nav" href="#">Association</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link title-f background-nav" href="#">événement</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link title-f background-nav" href="#">Actualité</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link title-f background-nav" href="#">Contactez-nous</a>
                 </li>
             </ul>
         </nav>   
@@ -18,6 +33,24 @@
     <main>
         <?= $content ?>
     </main>
+    <footer>
+        <div class="container-fluid text-center mt-5">
+            <div class="row">
+                <div class="col">
+                    Mentions légales :
+                </div>
+                <div class="col">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At officia labore dicta tenetur incidunt? Facilis, voluptates delectus, eligendi nesciunt sint, hic suscipit tempore nisi iste quia iure iusto enim ab.
+                </div>
+                <div class="col">
+                    <i class="fa-brands fa-facebook fa-2xl"></i>
+                    <i class="fa-solid fa-comment fa-2xl ms-3 me-3"></i>
+                    <i class="fa-solid fa-map-location-dot fa-2xl"></i>
+                </div>
+            </div>
+            <div class="row" id="footer"></div>
+        </div>  
+    </footer>
 <?php require('../assets/lib/externalsJS.php') ?>
 </body>
 </html>
